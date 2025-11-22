87a509f71469c3e8c7e98a0cfe8b5d3ea95dbe0a
Brent <brentjames.aberion@cit.edu>
2025-11-22 16:41:46 +0800

    added images

---
+++ Components/Pages/Products.cs
@@ -0,0 +1,6 @@
+public class Product
+{
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public int Quantity { get; set; } = 1;
+}
--- obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6127db7d0a4bfb76c448fb16c0f17c1a804c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b299e1a93e7af691a5193c9902e85adf821f5d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
