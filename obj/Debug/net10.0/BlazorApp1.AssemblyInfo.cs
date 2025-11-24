a02b550fad2d89ec436f08406e07acbe63a40e69
jay-ui12 <jayandrewclamares@gmail.com>
2025-11-24 13:16:58 +0800

    change servings number

--- obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a509f71469c3e8c7e98a0cfe8b5d3ea95dbe0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2e379c67002d85d66162ccadc2299db0292ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
