1eb4837b86818251df2207a3dd42db296d0e458c
jay-ui12 <jayandrewclamares@gmail.com>
2025-12-06 14:55:33 +0800

    added total price

--- Components/Services/CartService.cs
+++ Components/Services/CartService.cs
@@ -2,7 +2,10 @@ public class CartService
 {
     public List<CartItem> Items = new List<CartItem>();
 
-    public void AddToCart(string name, int quantity, string image)
+    // Read-only computed total (sum of price * quantity)
+    public int Total => Items.Sum(i => i.Price * i.Quantity);
+
+    public void AddToCart(string name, int quantity, string image, int price)
     {
         var existing = Items.FirstOrDefault(x => x.Name == name);
 
@@ -16,7 +19,8 @@ public class CartService
             {
                 Name = name,
                 Quantity = quantity,
-                Image = image
+                Image = image,
+                Price = price
             });
         }
     }
@@ -27,4 +31,5 @@ public class CartItem
     public string Name;
     public int Quantity;
     public string Image;
+    public int Price;
 }
